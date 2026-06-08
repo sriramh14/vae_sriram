f8c01231cf51ddac91224ce52fb995715cb550c2
sriramh14 <sriramhari1014@gmail.com>
2026-06-08 13:39:19 +0530

    Create reparam.py

---
+++ models/reparam.py
@@ -0,0 +1,8 @@
+class Reparameterization(nn.Module):
+    def forward(self, mu, logvar):
+
+        std = torch.exp(0.5 * logvar)
+
+        eps = torch.randn_like(std)
+
+        return mu + eps * std
