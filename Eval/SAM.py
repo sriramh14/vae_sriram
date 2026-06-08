605794549fd5744a7a5842b160cb73b2aebb9837
sriramh14 <sriramhari1014@gmail.com>
2026-06-08 13:40:42 +0530

    Create SAM.py

---
+++ Eval/SAM.py
@@ -0,0 +1,19 @@
+def sam_loss(pred, target):
+
+    B, C, H, W = pred.shape
+
+    pred = pred.reshape(B, C, -1)
+    target = target.reshape(B, C, -1)
+
+    dot = (pred * target).sum(dim=1)
+
+    pred_norm = torch.norm(pred, dim=1)
+    target_norm = torch.norm(target, dim=1)
+
+    cos = dot / (
+        pred_norm * target_norm + 1e-8
+    )
+
+    cos = torch.clamp(cos, -1, 1)
+
+    return torch.mean(torch.acos(cos))
