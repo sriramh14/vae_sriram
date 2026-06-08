1178abc53d1b6ed6fb213b1a08c211cc9b2d684c
sriramh14 <sriramhari1014@gmail.com>
2026-06-08 17:02:42 +0530

    Update Dataset_loader.py

--- dataset_loader/Dataset_loader.py
+++ dataset_loader/Dataset_loader.py
@@ -171,4 +171,15 @@ class ARADDataset(Dataset):
             cube
         ).float()
 
+        cube = torch.from_numpy(
+            cube
+        ).float()
+
+        cube = F.interpolate(
+            cube.unsqueeze(0),
+            size=(256, 256),
+            mode="bilinear",
+            align_corners=False
+        ).squeeze(0)
+
         return cube
