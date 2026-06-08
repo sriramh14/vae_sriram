f35c5b474f61597164e246acd27eb4eeb61dab65
sriramh14 <sriramhari1014@gmail.com>
2026-06-08 23:16:49 +0530

    Update resblock.py
    Modified definition

--- models/resblock.py
+++ models/resblock.py
@@ -1,16 +1,20 @@
 import torch
 import torch.nn as nn
 
-
-class ResidualBlock(nn.Module):
+class ResBlock(nn.Module):
     def __init__(self, channels):
         super().__init__()
 
         self.block = nn.Sequential(
             nn.Conv2d(channels, channels, 3, padding=1),
-            nn.GELU(),
-            nn.Conv2d(channels, channels, 3, padding=1)
+            nn.BatchNorm2d(channels),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(channels, channels, 3, padding=1),
+            nn.BatchNorm2d(channels)
         )
 
+        self.relu = nn.ReLU(inplace=True)
+
     def forward(self, x):
-        return x + self.block(x)
+        return self.relu(x + self.block(x))
