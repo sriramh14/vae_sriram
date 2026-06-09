5bfa6e98b51bae6abd07a2390e58d7c2a42e7396
sriramh14 <sriramhari1014@gmail.com>
2026-06-10 00:52:38 +0530

    Update RGBEncoder.py

--- models/RGBEncoder.py
+++ models/RGBEncoder.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
+
 from .resblock import ResidualBlock
+
+
 class RGBEncoder(nn.Module):
 
     def __init__(
@@ -12,23 +15,36 @@ class RGBEncoder(nn.Module):
 
         self.net = nn.Sequential(
 
-            nn.Conv2d(in_channels, 64, 4, stride=2, padding=1),
+            nn.Conv2d(
+                in_channels,
+                64,
+                kernel_size=4,
+                stride=2,
+                padding=1
+            ),
             nn.ReLU(inplace=True),
 
             ResidualBlock(64),
 
-            nn.Conv2d(64, 128, 4, stride=2, padding=1),
+            nn.Conv2d(
+                64,
+                128,
+                kernel_size=4,
+                stride=2,
+                padding=1
+            ),
             nn.ReLU(inplace=True),
 
             ResidualBlock(128),
 
-            nn.Conv2d(128, 256, 4, stride=2, padding=1),
-            nn.ReLU(inplace=True),
-
-            ResidualBlock(256),
-
-            nn.Conv2d(256, latent_channels, 3, padding=1)
+            nn.Conv2d(
+                128,
+                latent_channels,
+                kernel_size=3,
+                padding=1
+            )
         )
 
     def forward(self, x):
+
         return self.net(x)
